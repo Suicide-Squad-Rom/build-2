c804af6c6afa1640c873b5edc6f11b66ce43d527
Adarsh-MR <adarshmr1998@gmail.com>
2016-04-21 08:14:08 +0200

    Revert "Modifiy for aosp"
    This reverts commit 49b6509aa0fe80e00bb67e833f46f8ad73dfdcfd.

--- core/config.mk
+++ core/config.mk
@@ -723,9 +723,10 @@ ifneq ($(TARGET_COPY_FILES_OVERRIDES),)
     PRODUCT_COPY_FILES := $(filter-out $(TARGET_COPY_FILES_OVERRIDES), $(PRODUCT_COPY_FILES))
 endif
 
+ifneq ($(SLIM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/aosp/sepolicy/sepolicy.mk)
+$(eval include vendor/slim/sepolicy/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
@@ -733,5 +734,6 @@ $(eval include vendor/aosp/sepolicy/sepolicy.mk)
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
+endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
